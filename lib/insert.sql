7d698937c5c65763d01dc385cf0a1f44ff9697ac
Sav Younts <savyounts@gmail.com>
2018-08-22 18:26:06 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,24 +1,24 @@
 
  /*projects */
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("garden", "house", 100, "Oct 1", "Dec 1");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("garden", "house", 50, "Oct 1", "Dec 1");
 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("garden", "house", 100, "Oct 1", "Dec 1");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("closet", "build", 100, "Oct 1", "Dec 1");
 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("garden", "house", 100, "Oct 1", "Dec 1");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("repairs", "car", 124, "Oct 1", "Dec 1");
 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("garden", "house", 100, "Oct 1", "Dec 1");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("broken leg", "health", 1000, "Oct 1", "Dec 1");
 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("garden", "house", 100, "Oct 1", "Dec 1");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("chair", "build", 30, "Oct 1", "Dec 1");
 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("garden", "house", 100, "Oct 1", "Dec 1");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("new tire", "car", 600, "Oct 1", "Dec 1");
 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("garden", "house", 100, "Oct 1", "Dec 1");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("paint job", "house", 400, "Oct 1", "Dec 1");
 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("garden", "house", 100, "Oct 1", "Dec 1");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("cabinet", "build", 100, "Oct 1", "Dec 1");
 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("garden", "house", 100, "Oct 1", "Dec 1");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("stolen car", "car", 5000, "Oct 1", "Dec 1");
 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("garden", "house", 100, "Oct 1", "Dec 1");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("table", "build", 50, "Oct 1", "Dec 1");
 
 /* users*/
 

0ec61f7f2a2e02e93e334f40a7f6e5ec082bc716
Sav Younts <savyounts@gmail.com>
2018-08-22 18:22:43 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE projects(
-  id PRIMARY KEY INTEGER,
+  id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
   funding_goal INTEGER,
@@ -8,13 +8,13 @@ CREATE TABLE projects(
 );
 
 CREATE TABLE users(
-  id PRIMARY KEY INTEGER,
+  id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER
 );
 
 CREATE TABLE pledges(
-  id PRIMARY KEY INTEGER,
+  id INTEGER PRIMARY KEY,
   amount INTEGER,
   user_id INTEGER,
   project_id INTEGER

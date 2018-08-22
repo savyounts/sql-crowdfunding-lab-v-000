47b2e0ea1204d680524891193e3ec661a03dfad2
Sav Younts <savyounts@gmail.com>
2018-08-22 18:53:20 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,7 +28,10 @@ HAVING SUM(amount) >= projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT users.name, SUM(amount) FROM pledges
+JOIN users ON users.id = pledges.user_id
+GROUP BY user.id
+ORDER BY user.name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category

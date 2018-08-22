baef1ca1f057f99c596aee19bbf9a7b37f6056df
Sav Younts <savyounts@gmail.com>
2018-08-22 18:34:57 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "Select project.name, SUM(amount) FROM projects
 JOIN project.id = pledges.project_id
-Group By project.name"
+Group By project.name;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
